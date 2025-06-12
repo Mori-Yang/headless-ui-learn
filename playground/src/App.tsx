0ad78d67a208ffc7626b75a0643d75646f8c5697
Mori-Yang <ys0514@yeah.net>
2025-06-13 01:40:17 +0800

    asChild prop

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -8,6 +8,8 @@ import './index.less';
 //     { text: '3', description: '3333' },
 
 // ];
+// 支持自定义样式的方式
+// 1. as prop
 const HeadlessBtnAs = ({ as: Comp = 'button' }: { as?: ElementType }) => {
     return <Comp>123</Comp>;
 };
@@ -15,27 +17,35 @@ const MyBtn = () => {
     return <>123</>;
 };
 
+// 2. render prop，使用children作为render prop
 const HeadlessBtnRP = ({ children }: { children: (count: number, setCount: React.Dispatch<React.SetStateAction<number>>) => ReactNode }) => {
     const [count, setCount] = useState(0);
     return children(count, setCount);
+};
+
+// 3. asChild prop
+const HeadlessBtnAsChild = ({ asChild = false, children }: { asChild?: boolean, children?: ReactNode }) => {
+    if (asChild) {
+        return children;
+    }
+    return <></>;
 };
 
 const App = () => {
     return (
         <div id="container">
             {/* <Dropdown items={items} /> */}
-            <HeadlessBtnAs as="div" />
+            <HeadlessBtnAs as="a" />
             <HeadlessBtnAs as={MyBtn} />
             <HeadlessBtnRP>
                 {(count, setCount) => <div onClick={() => { setCount(count++); }}>{count}</div>}
             </HeadlessBtnRP>
-
+            <HeadlessBtnAsChild asChild>
+                <a href="/">asChild Btn</a>
+            </HeadlessBtnAsChild>
             <Menu>
                 <MenuButton>1</MenuButton>
-                <MenuItems onSelect={(a) => {
-                    console.log(a);
-                }}
-                >
+                <MenuItems>
                     <MenuItem>
                         <p>Settings1</p>
                     </MenuItem>
